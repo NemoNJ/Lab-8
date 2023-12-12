f6541b3f36c9ef438f6b3f2fe0175ff6d4d3d14c
NemoNJ <nemojirayu@gmail.com>
2023-12-12 23:00:20 +0700

    finish

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,44 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+double age ,height ,bounty ;
+cout << "Enter your age: ";
+cin >> age;
+if( age <= 25 ){
+cout << "Enter your height: ";
+cin >> height;
+   if( height < 100 ){
+   cout << "Your character = Chopper";
+   }
+   else if(height >= 100 && height < 180){
+   cout << "Your character = Usopp";
+   }
+   else{
+   cout << "Enter your bounty: ";
+   cin >> bounty;
+   if(bounty > 1100000000){
+   cout << "Your character = Zoro";
+   }
+   else{
+   cout << "Your character = Sanji";
+   }
+   }
+}
+else if( age > 25  && age <= 60 ){
+   cout << "Enter your bounty: ";
+   cin >> bounty;
+   if(bounty > 500000000){
+   cout << "Your character = Jinbe";
+   }
+   else{
+   cout << "Your character = Franky";
+   }
+}
+else if(age > 60 ){
+cout << "Your character = Brook";
+}
+}
